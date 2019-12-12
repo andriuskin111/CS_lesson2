621031f8c99bbbf6de8412cca81a494839b75ac9
andriuskin111 <andriuskin@gmail.com>
2019-12-12 21:58:56 +0200

    Ketvirta_uzduotis_v01

--- Ketvirta uzduotis/Ketvirta uzduotis/Program.cs
+++ Ketvirta uzduotis/Ketvirta uzduotis/Program.cs
@@ -10,6 +10,36 @@ namespace Ketvirta_uzduotis
     {
         static void Main(string[] args)
         {
+            string mokyklosPavadinimas;
+            string kursoPabadinimas;
+            int studentuSkaicius;
+            DateTime kursoPradziosData = new DateTime(2019, 12, 10);
+            DateTime kursoPabaigosData = new DateTime(2020, 2, 25);
+            string dienuSkirtumas = (kursoPabaigosData - kursoPradziosData).TotalDays.ToString();
+
+            //(EndDate - StartDate).TotalDays
+
+            //int dienuSkirtumas = 77;
+            //int savaiciuSkaicius = Convert.ToInt32
+            //int dienuSkaiciusBeSavaitgaliu = 44;
+
+            Console.WriteLine("Iveskite mokyklos pavadinima");
+            mokyklosPavadinimas = Console.ReadLine();
+            Console.WriteLine("Iveskite kursopavadinima");
+            kursoPabadinimas = Console.ReadLine();
+            Console.WriteLine("Iveskite studentu skaiciu");
+            studentuSkaicius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Mokyklos pavadinimas: " + mokyklosPavadinimas);
+            Console.WriteLine("Kurso pavadinimas: " + kursoPabadinimas);
+            Console.WriteLine("Stdentu skaicius: " + studentuSkaicius);
+            Console.WriteLine("Siandienos data: " + DateTime.Now.ToString());
+            Console.WriteLine("Kurso pradzios data: " + kursoPradziosData);
+            Console.WriteLine("Kurso pabaigos data: " + kursoPabaigosData);
+            Console.WriteLine("Dienu skirtumas tarp kuso pradzios ir pabaigos: " + dienuSkirtumas);
+            //Console.WriteLine("Savaiciu skaicius: " + savaiciuSkaicius);
+            Console.WriteLine("");
+            Console.ReadLine();
         }
     }
 }
