92dc42b63c836aff9c9b63d035a9750b386a1065
andriuskin111 <andriuskin@gmail.com>
2019-12-12 19:12:27 +0200

    1 uzduotis

--- Lesson2 start/Lesson2 start/Program.cs
+++ Lesson2 start/Lesson2 start/Program.cs
@@ -10,6 +10,20 @@ namespace Lesson2_start
     {
         static void Main(string[] args)
         {
+            double pirmasSkaicius;
+            double antrasSkaicius;
+
+
+            Console.WriteLine("Iveskite pima skaiciu");
+            pirmasSkaicius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Iveskite antra skaiciu");
+            antrasSkaicius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Skaiciu suma yra: " + (pirmasSkaicius + antrasSkaicius));
+            Console.WriteLine("Skaiciu skirtumas yra: " + (pirmasSkaicius - antrasSkaicius));
+            Console.WriteLine("Skaiciu sandauga yra: " + (pirmasSkaicius * antrasSkaicius));
+            Console.WriteLine("Skaiciu dalyba yra: " + (pirmasSkaicius / antrasSkaicius));
+            Console.ReadLine();
         }
     }
 }
