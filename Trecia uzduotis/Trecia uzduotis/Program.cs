ce785068ce0a82334912f0559d4d6bdf5c259f52
andriuskin111 <andriuskin@gmail.com>
2019-12-12 19:45:16 +0200

    Trecia_uzduotis_v01

--- Trecia uzduotis/Trecia uzduotis/Program.cs
+++ Trecia uzduotis/Trecia uzduotis/Program.cs
@@ -10,6 +10,23 @@ namespace Trecia_uzduotis
     {
         static void Main(string[] args)
         {
+            string mokyklosPavadinimas;
+            string kursoPabadinimas;
+            int studentuSkaicius;
+            DateTime.Today.ToString();
+
+            Console.WriteLine("Iveskite mokyklos pavadinima");
+            mokyklosPavadinimas = Console.ReadLine();
+            Console.WriteLine("Iveskite kursopavadinima");
+            kursoPabadinimas = Console.ReadLine();
+            Console.WriteLine("Iveskite studentu skaiciu");
+            studentuSkaicius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Mokyklos pavadinimas: " + mokyklosPavadinimas);
+            Console.WriteLine("Kurso pavadinimas: " + kursoPabadinimas);
+            Console.WriteLine("Stdentu skaicius: " + studentuSkaicius);
+            Console.WriteLine(DateTime.Now.ToString());
+            Console.ReadLine();
         }
     }
 }
