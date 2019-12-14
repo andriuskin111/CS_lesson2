c003fe835513fc0ed52508cf8f11cd64a00f9af7
andriuskin111 <andriuskin@gmail.com>
2019-12-15 01:43:37 +0200

    Nd4_v02

--- Nd4/Nd4/Program.cs
+++ Nd4/Nd4/Program.cs
@@ -10,6 +10,16 @@ namespace Nd4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite pirma skaiciu");
+            double PirmasSkaicius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Iveskite antra skaiciu");
+            double AntrasSkaicius = Convert.ToDouble(Console.ReadLine());
+
+            PirmasSkaicius++;
+            AntrasSkaicius--;
+
+            Console.WriteLine("Pirmas skaicius + 1: {0}, Antras skaicius - 1: {1}", PirmasSkaicius, AntrasSkaicius);
+            Console.ReadLine();
         }
     }
 }
