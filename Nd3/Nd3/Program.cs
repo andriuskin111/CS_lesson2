0d1feae3c611827275f567e8f14f12069d192798
andriuskin111 <andriuskin@gmail.com>
2019-12-15 01:36:02 +0200

    Nd3_v02

--- Nd3/Nd3/Program.cs
+++ Nd3/Nd3/Program.cs
@@ -9,7 +9,24 @@ namespace Nd3
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            Console.WriteLine("Iveskite pirma skaiciu");
+            double PirmasSkaicius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Iveskite antra skaiciu");
+            double AntrasSkaicius = Convert.ToDouble(Console.ReadLine());
+
+            if(PirmasSkaicius == AntrasSkaicius)
+            {
+                Console.WriteLine("Skaiciai yra lygus");
+            }
+            else
+            {
+                Console.WriteLine("Skaiciai yra nelygus");
+            }
+
+            Console.ReadLine();
+            
         }
+
     }
 }
