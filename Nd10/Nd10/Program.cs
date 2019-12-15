338db77ec4018c800d451c996e86a0b1d00f8457
andriuskin111 <andriuskin@gmail.com>
2019-12-15 23:06:34 +0200

    Nd10_v02

--- Nd10/Nd10/Program.cs
+++ Nd10/Nd10/Program.cs
@@ -10,6 +10,33 @@ namespace Nd10
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite miesto pavadinima");
+            string miestoPavadinimas = Console.ReadLine();
+            Console.WriteLine("Iveskite gatves pavadinima");
+            string gatvesPavadinimas = Console.ReadLine();
+            Console.WriteLine("Iveskite namo numeri");
+            string namoNumeris = Console.ReadLine();
+            Console.WriteLine("Iveskite statymo pradzios data (pvz.: 2018/10/10)");
+            DateTime ivestiStatymoPradziosMetai = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Iveskite pastatymo data (pvz.: 2018/10/10)");
+            DateTime ivestiPastatymoMetai = DateTime.Parse(Console.ReadLine());
+
+            double statybosLaikotarpisDienom = (ivestiPastatymoMetai - ivestiStatymoPradziosMetai).TotalDays;
+            double statybosLaikotarpisMetais = statybosLaikotarpisDienom / 365;
+
+            RodytiDuomenis(miestoPavadinimas, gatvesPavadinimas, namoNumeris);
+
+            Console.ReadLine();
+
+            void RodytiDuomenis(string aMiestoPavadinimas, string aGatvesPavadinimas, string aNamoNumeris)
+            {
+                Console.WriteLine("Miestas: {0}", aMiestoPavadinimas, aGatvesPavadinimas, aNamoNumeris);
+                Console.WriteLine("Gatve: {1}", aMiestoPavadinimas, aGatvesPavadinimas, aNamoNumeris);
+                Console.WriteLine("Namo Nr: {2}", aMiestoPavadinimas, aGatvesPavadinimas, aNamoNumeris);
+                Console.WriteLine("Statymo pradzios metai: {0}", ivestiStatymoPradziosMetai);
+                Console.WriteLine("Pastatymo metai: {0}", ivestiPastatymoMetai);
+                Console.WriteLine("Statybos laikotarpis: {0}", statybosLaikotarpisMetais);
+            }
         }
     }
 }
