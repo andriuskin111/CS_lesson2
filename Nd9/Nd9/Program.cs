de4755b3e6ff69877c1e41358c8cf8e76592bba4
andriuskin111 <andriuskin@gmail.com>
2019-12-15 22:45:37 +0200

    Nd9_v02

--- Nd9/Nd9/Program.cs
+++ Nd9/Nd9/Program.cs
@@ -10,6 +10,26 @@ namespace Nd9
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite miesto pavadinima");
+            string miestoPavadinimas = Console.ReadLine();
+            Console.WriteLine("Iveskite gatves pavadinima");
+            string gatvesPavadinimas = Console.ReadLine();
+            Console.WriteLine("Iveskite namo numeri");
+            int namoNumeris = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite pastatymo data (pvz.: 2019/10/10)");
+            DateTime ivestiPastatymoMetai = DateTime.Parse(Console.ReadLine());
+
+            RodytiDuomenis(miestoPavadinimas, gatvesPavadinimas, namoNumeris);
+
+            Console.ReadLine();
+
+            void RodytiDuomenis(string aMiestoPavadinimas, string aGatvesPavadinimas, int aNamoNumeris)
+            {
+                Console.WriteLine("Miestas: {0}", aMiestoPavadinimas, aGatvesPavadinimas, aNamoNumeris);
+                Console.WriteLine("Gatve: {1}", aMiestoPavadinimas, aGatvesPavadinimas, aNamoNumeris);
+                Console.WriteLine("Namo Nr: {2}", aMiestoPavadinimas, aGatvesPavadinimas, aNamoNumeris);
+                Console.WriteLine("Pastatymo metai: {0}", ivestiPastatymoMetai);
+            }
         }
     }
 }
