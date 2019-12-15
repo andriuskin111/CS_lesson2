fb18cdf731ba2e8d1927a5df1742e99fa846e2ae
andriuskin111 <andriuskin@gmail.com>
2019-12-15 02:40:55 +0200

    Nd7_02

--- Nd7/Nd7/Program.cs
+++ Nd7/Nd7/Program.cs
@@ -10,6 +10,26 @@ namespace Nd7
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite skaiciu");
+            double skaicius = Convert.ToDouble(Console.ReadLine());
+
+            Palyginti(skaicius);
+
+            Console.WriteLine("Skaicius mazesnis uz 0: " + Palyginti(skaicius));
+            Console.ReadLine();
+
+            bool Palyginti(double aSkaicius)
+            {
+                if(aSkaicius < 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
+
     }
 }
